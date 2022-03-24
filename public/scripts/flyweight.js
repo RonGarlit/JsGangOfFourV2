1f43674edeb1af9b69017ec9d4313a359897992c
Ronald Garlit <rongarlit@live.com>
2022-03-24 13:32:51 -0400

    Restart Project and test Github

--- public/scripts/flyweight.js
+++ public/scripts/flyweight.js
@@ -1,7 +1,6 @@
 /*
 Use sharing to support large numbers of fine-grained objects efficiently. 
 
-
 The Flyweight pattern conserves memory by sharing large numbers of fine-grained objects efficiently. Shared flyweight objects are immutable, that is, they cannot be changed as they represent the characteristics that are shared with other objects.
 
 Essentially Flyweight is an 'object normalization technique' in which common properties are factored out into shared flyweight objects. (Note: the idea is similar to data model normalization, a process in which the modeler attempts to minimize redundancy).
@@ -9,7 +8,6 @@ Essentially Flyweight is an 'object normalization technique' in which common pro
 An example of the Flyweight Pattern is within the JavaScript engine itself which maintains a list of immutable strings that are shared across the application.
 
 Other examples include characters and line-styles in a word processor, or 'digit receivers' in a public switched telephone network application. You will find flyweights mostly in utility type applications such as word processors, graphics programs, and network apps; they are less often used in data-driven business type applications.
-
 
 */
 
