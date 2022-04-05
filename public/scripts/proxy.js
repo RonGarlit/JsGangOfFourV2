506e64e0ec9d41f54d0a3214282991799b0227b1
RonGarlit <rongarlit@live.com>
2022-04-04 21:09:40 -0400

    Finished Proxy Pattern

--- public/scripts/proxy.js
+++ public/scripts/proxy.js
@@ -77,3 +77,112 @@ function run_proxy() {
     log.add("\nCache size: " + geo.getCount());
     log.show();
 }
+
+//=============================================
+// Proxy NameSpace 
+//=============================================
+// JavaScript does not support class-based 
+// inheritance or abstract classes.
+// we must ensure this consistency ourselves
+// that properties and methods match!!!
+//==============================================
+// Create our namespace called "JsGangOfFour".
+//
+// This is based on the NameSpace Pattern and 
+// Module Pattern. This block of code should 
+// be at the top of separate files to better 
+// manage codebase.
+//
+// More importantly creating a structure to 
+// avoid name collisions globally under one varable
+// called JsGangOfFour.
+//==============================================
+var JsGangOfFour = {
+    namespace: function (name) {
+        var parts = name.split(".");
+        var ns = this;
+
+        for (var i = 0, len = parts.length; i < len; i++) {
+            ns[parts[i]] = ns[parts[i]] || {};
+            ns = ns[parts[i]];
+        }
+
+        return ns;
+    }
+};
+
+//==============================================
+// Create the "Classic" namespace 
+//==============================================
+JsGangOfFour.namespace("Classic").Proxy = (function () {
+
+    var Geo = function () {
+        var geocoder = new GeoCoder();
+        var geocache = {};
+
+        return {
+            getLatLng: function (address) {
+                if (!geocache[address]) {
+                    geocache[address] = geocoder.getLatLng(address);
+                }
+
+                log.add(address + ": " + geocache[address]);
+                return geocache[address];
+            },
+            getCount: function () {
+                var count = 0;
+                for (var code in geocache) { count++; }
+                return count;
+            }
+        };
+    };
+
+    return {
+        Geo: Geo
+    };
+})();
+
+// Simulated Google Maps Geocoding Service
+function GeoCoder() {
+    this.getLatLng = function (address) {
+
+        if (address === "Amsterdam") {
+            return "52.3700° N, 4.8900° E";
+        } else if (address === "London") {
+            return "51.5171° N, 0.1062° W";
+        } else if (address === "Paris") {
+            return "48.8742° N, 2.3470° E";
+        } else if (address === "Berlin") {
+            return "52.5233° N, 13.4127° E";
+        } else {
+            return "";
+        }
+    };
+}
+
+//=============================================
+// run_OptimizedProxy()
+//=============================================
+function run_OptimizedProxy() {
+
+    var proxy = JsGangOfFour.Classic.Proxy;
+
+    var geo = new proxy.Geo();
+
+    // geolocation requests
+    geo.getLatLng("Paris");
+    geo.getLatLng("London");
+    geo.getLatLng("London");
+    geo.getLatLng("London");
+    geo.getLatLng("London");
+    geo.getLatLng("Amsterdam");
+    geo.getLatLng("Amsterdam");
+    geo.getLatLng("Amsterdam");
+    geo.getLatLng("Amsterdam");
+    geo.getLatLng("London");
+    geo.getLatLng("London");
+
+    log.add("\nCache size: " + geo.getCount());
+    log.show();
+
+}
